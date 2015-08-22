a2c833ffda93da34d37f4484482e12e331c8fcda
Farsheed Ashouri <rodmena@me.com>
2015-08-22 21:41:02 +0000

    bfix

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ Clean code is much better than Cleaner comments!
 @author: F.Ashouri
 """
 
-#from gevent import monkey
-#monkey.patch_all()
+from gevent import monkey
+monkey.patch_all()
 
 from flask import Flask, request, abort, jsonify
 from flask.ext.mako import render_template
@@ -329,7 +329,7 @@ if __name__ == "__main__":
 	import bjoern
 	bjoern.run(app, '0.0.0.0', 9000)
 
-    run_tornado()
-    #run_debug()
+    #run_tornado()
+    run_debug()
     #run_bjoern()
     # run_gevent()
